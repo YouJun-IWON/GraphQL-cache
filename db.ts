8f9a394df1d8fba67e2a696b9cdf829d8c282123
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-09-06 18:19:40 +0900

    learning graphql cache

---
+++ db.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  todos: [
+    {
+      id: 0,
+      text: '밥먹기',
+      checked: false,
+    },
+    {
+      id: 1,
+      text: '밥먹rl기',
+      checked: false,
+    },
+  ],
+};
